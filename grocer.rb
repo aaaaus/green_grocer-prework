8e2bf7b5597eea6314e1054d13077ba8fe1deb24
Austin Luft <ausluft@gmail.com>
2018-10-04 23:15:13 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def consolidate_cart(cart)
   hash = {}
   cart.each do | element |
@@ -15,22 +17,24 @@ end
 
 def apply_coupons(cart, coupons)
 
-  result = {}
-  # code here#
-  cart.each do |food, info|
-    coupons.each do |coupon|
-      if food == coupon[:item] && info[:count] >= coupon[:num]
-        info[:count] =  info[:count] - coupon[:num]
-        if result["#{food} W/COUPON"]
-          result["#{food} W/COUPON"][:count] += 1
-        else
-          result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
-        end
-      end
-    end
-    result[food] = info
-  end
-  result
+binding.pry
+
+  # result = {}
+  # # code here#
+  # cart.each do |food, info|
+  #   coupons.each do |coupon|
+  #     if food == coupon[:item] && info[:count] >= coupon[:num]
+  #       info[:count] =  info[:count] - coupon[:num]
+  #       if result["#{food} W/COUPON"]
+  #         result["#{food} W/COUPON"][:count] += 1
+  #       else
+  #         result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+  #       end
+  #     end
+  #   end
+  #   result[food] = info
+  # end
+  # result
 
   #   coupons.each do | coupons_hash |
   #     coupon_item = coupons_hash[:item] 
