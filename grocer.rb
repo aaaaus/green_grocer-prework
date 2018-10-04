b10b1d65bf5d0fdf5cb300772effd793e647d36a
Austin Luft <ausluft@gmail.com>
2018-10-04 23:11:19 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,38 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do | element |
+    element.each do | name , values |
+      if hash.has_key?(name) == false
+        hash[name] = values
+        hash[name][:count] = 1
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  return hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+    coupons.each do | coupons_hash |
+      coupon_item = coupons_hash[:item] 
+      new_coupon_hash = {
+        :price => coupons_hash[:cost],
+        :clearance => true,
+        :count => coupons_hash[:num]
+      }
+    
+    if cart.key?(coupon_item)
+      new_coupon_hash[:clearance] = cart[coupon_item][:clearance]
+      if cart[coupon_item][:count] >= new_coupon_hash[:count]
+        new_coupon_hash[:count] = (cart[coupon_item][:count] / new_coupon_hash[:count]).floor
+        cart[coupon_item][:count] = coupons_hash[:num] % cart[coupon_item][:count]
+      end
+      cart[coupon_item + " W/COUPON"] = new_coupon_hash
+    end
+  end
+  return cart
 end
 
 def apply_clearance(cart)
