e59f0a692a9a628ec82c52902298cc573267e9d2
Austin Luft <ausluft@gmail.com>
2018-10-04 23:13:52 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,24 +15,41 @@ end
 
 def apply_coupons(cart, coupons)
 
-    coupons.each do | coupons_hash |
-      coupon_item = coupons_hash[:item] 
-      new_coupon_hash = {
-        :price => coupons_hash[:cost],
-        :clearance => true,
-        :count => coupons_hash[:num]
-      }
-    
-    if cart.key?(coupon_item)
-      new_coupon_hash[:clearance] = cart[coupon_item][:clearance]
-      if cart[coupon_item][:count] >= new_coupon_hash[:count]
-        new_coupon_hash[:count] = (cart[coupon_item][:count] / new_coupon_hash[:count]).floor
-        cart[coupon_item][:count] = coupons_hash[:num] % cart[coupon_item][:count]
+  result = {}
+  # code here#
+  cart.each do |food, info|
+    coupons.each do |coupon|
+      if food == coupon[:item] && info[:count] >= coupon[:num]
+        info[:count] =  info[:count] - coupon[:num]
+        if result["#{food} W/COUPON"]
+          result["#{food} W/COUPON"][:count] += 1
+        else
+          result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+        end
       end
-      cart[coupon_item + " W/COUPON"] = new_coupon_hash
     end
+    result[food] = info
   end
-  return cart
+  result
+
+  #   coupons.each do | coupons_hash |
+  #     coupon_item = coupons_hash[:item] 
+  #     new_coupon_hash = {
+  #       :price => coupons_hash[:cost],
+  #       :clearance => true,
+  #       :count => coupons_hash[:num]
+  #     }
+    
+  #   if cart.key?(coupon_item)
+  #     new_coupon_hash[:clearance] = cart[coupon_item][:clearance]
+  #     if cart[coupon_item][:count] >= new_coupon_hash[:count]
+  #       new_coupon_hash[:count] = (cart[coupon_item][:count] / new_coupon_hash[:count]).floor
+  #       cart[coupon_item][:count] = coupons_hash[:num] % cart[coupon_item][:count]
+  #     end
+  #     cart[coupon_item + " W/COUPON"] = new_coupon_hash
+  #   end
+  # end
+  # return cart
 end
 
 def apply_clearance(cart)
